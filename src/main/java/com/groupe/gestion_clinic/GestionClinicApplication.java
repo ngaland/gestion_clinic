9b2e9c81d3ac66726a770e138a888104e7644080
ngaland <ngaland093@gmail.com>
2025-06-25 11:15:01 +0100

    Initial commit

---
+++ src/main/java/com/groupe/gestion_clinic/GestionClinicApplication.java
@@ -0,0 +1,13 @@
+package com.groupe.gestion_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClinicApplication.class, args);
+	}
+
+}
