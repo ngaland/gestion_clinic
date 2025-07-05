f3f1df8d5e2f235d021f02bd96a53a11088eecff
hels <m7hels@gmail.com>
2025-07-05 03:52:13 +0100

    add ConflictException

--- src/main/java/com/groupe/gestion_clinic/services/serviceImpl/RendezvousServiceImpl.java
+++ src/main/java/com/groupe/gestion_clinic/services/serviceImpl/RendezvousServiceImpl.java
@@ -1,7 +1,11 @@
 package com.groupe.gestion_clinic.services.serviceImpl;
 
+import com.groupe.gestion_clinic.repositories.RendezvousRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RendezvousServiceImpl {
+    private final RendezvousRepository rendezvousRepository;
 }
